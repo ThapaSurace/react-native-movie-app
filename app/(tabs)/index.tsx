d513f1c9a9e7e657ce45fd1331f654c2b7002247
Suresh Thapa <zorosenpai456@gmail.com>
2025-03-08 13:32:31 +0545

    home screen created

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,23 @@
-import { Text, View } from "react-native";
+import { StatusBar } from "react-native";
+import { icons } from "@/constants/icons";
+import { ScrollView, Text, View, Image } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl font-bold text-slate-900">
-        Edit app/index.tsx to edit this screen.
-      </Text>
+    <View className="flex-1 bg-primary">
+      {/* Set the status bar style */}
+      <StatusBar barStyle="light-content" backgroundColor="#0f172b" />
+
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          minHeight: "100%",
+          marginBottom: 10,
+        }}
+      >
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+      </ScrollView>
     </View>
   );
 }
