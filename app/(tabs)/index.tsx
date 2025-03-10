f405db17f1ff415344fe7792bdefee5f737af351
Suresh Thapa <zorosenpai456@gmail.com>
2025-03-10 08:19:53 +0545

    fetched movie using useFetch hook and display movies using flat list

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,26 @@
-import { StatusBar } from "react-native";
+import { FlatList, StatusBar } from "react-native";
 import { icons } from "@/constants/icons";
 import { ScrollView, Text, View, Image } from "react-native";
+import SearchBar from "@/components/SearchBar";
+import { useRouter } from "expo-router";
+import useFetch from "@/services/useFetch";
+import { fetchPopularMovies } from "@/services/api";
+import MovieCard from "@/components/MovieCard";
+import AppLogo from "@/components/AppLogo";
 
 export default function Index() {
+  const router = useRouter();
+
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() =>
+    fetchPopularMovies({
+      query: "",
+    })
+  );
+
   return (
     <View className="flex-1 bg-primary">
       {/* Set the status bar style */}
@@ -16,7 +34,37 @@ export default function Index() {
           marginBottom: 10,
         }}
       >
-        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+        <View className="mt-4 mb-2">
+          <AppLogo />
+        </View>
+        <View className="flex-1">
+          <SearchBar
+            onPress={() => router.push("/search")}
+            placeholder="Search for a movie"
+            value=""
+            onChangeText={() => {}}
+          />
+
+          <FlatList
+            data={movies}
+            renderItem={({ item }) => <MovieCard {...item} />}
+            keyExtractor={(item) => item.id.toString()}
+            numColumns={3}
+            columnWrapperStyle={{
+              justifyContent: "flex-start",
+              gap: 20,
+              paddingRight: 5,
+              marginBottom: 10,
+            }}
+            className="mt-2 pb-32"
+            scrollEnabled={false}
+            ListHeaderComponent={
+              <Text className="text-lg font-semibold text-gray-100 mb-4 mt-4">
+                Popular Movies
+              </Text>
+            }
+          />
+        </View>
       </ScrollView>
     </View>
   );
